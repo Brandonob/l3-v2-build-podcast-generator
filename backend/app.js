249ac4666ad324b5e9df601c7817601640d107f7
BrandonBrown <brandonbrown@pursuit.org>
2025-01-31 00:37:39 -0500

    chore: set up the request for handling file uploads

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,55 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
+const multer = require('multer');
+const fs = require('fs');
 
 const PORT = process.env.PORT;
 const app = express();
+const genAi = new GoogleGenerativeAI(process.env.AI_APIKEY);
+
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/api/textToAudio', async (req, res) => {
-  const { transcript } = req.body;
+app.post('/api/processAudio', upload.single('audio'), async (req, res) => {
+  // const { transcript } = req.body;
   try {
-  } catch (error) {}
+    if (!req.file) {
+      return res.status(400).json({ error: 'No audio file uploaded' });
+    }
+
+    const audioFile = req.file;
+
+    debugger;
+
+    const model = genAi.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+    });
+
+    const prompt = `Generate a transcript of the speech.`;
+    const result = await model.generateContent([
+      {
+        fileData: {
+          mimeType: audioFile.mimeType,
+          fileUri: audioFile.uri,
+        },
+      },
+      { text: prompt },
+    ]);
+
+    const audioTranscript = result.response.text();
+
+    debugger;
+    res.json({
+      audioTranscript: audioTranscript,
+    });
+  } catch (error) {
+    console.log('Failed to process audio error', error);
+    res.status(500).json({ error: 'Failed to process audio' });
+  }
 });
 
 app.listen(PORT, () => {
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -14,12 +14,28 @@ import {
 export default function Home() {
   const [text, setText] = useState('');
   const [isSpeaking, setIsSpeaking] = useState(false);
-
   const [file, setFile] = useState(null);
+  const [isAudioUpload, setIsAudioUpload] = useState(false);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
+
+  const handleFileUpload = async () => {
+    if (!file) {
+      alert('Please select an audio file.');
+      return;
+    }
+
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('audio', file);
+
+    try {
+    } catch (error) {}
+  };
+
+  const handleFormChange = () => {};
 
   const handleSubmit = async (e) => {
     //if transcript execute transcript webspeech logic
@@ -27,14 +43,14 @@ export default function Home() {
     e.preventDefault();
     // Handle form submission logic
     const formData = new FormData();
-    formData.append('text', text);
+    // formData.append('text', text);
     // formData.append('file', file);
-    debugger;
+    // debugger;
 
     const speech = new SpeechSynthesisUtterance(text);
 
     speech.onstart = () => setIsSpeaking(true);
-    speech.onstart = () => setIsSpeaking(true);
+    speech.onend = () => setIsSpeaking(false);
 
     window.speechSynthesis.speak(speech);
 
@@ -54,33 +70,49 @@ export default function Home() {
     <Box maxW='md' mx='auto' mt={8} p={4} borderWidth={1} borderRadius='md'>
       <form onSubmit={handleSubmit}>
         <VStack spacing={4}>
-          {/* show if transcript button is active */}
-          <FormControl id='text' isRequired>
-            <FormLabel>Text Input</FormLabel>
-            <Textarea
-              placeholder='Enter your text here'
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-            />
-          </FormControl>
-          {/* show if upload button is active */}
-          {/* <FormControl id='file' isRequired>
-            <FormLabel>File Upload</FormLabel>
-            <Input
-              type='file'
-              onChange={handleFileChange}
-              accept='.png, .jpg, .jpeg, .pdf' // Restrict file types if needed
-            />
-          </FormControl> */}
-
-          <Button
-            type='submit'
-            colorScheme='blue'
-            w='full'
-            disabled={isSpeaking}
-          >
-            {isSpeaking ? 'Speaking...' : 'Speak'}
+          <Button onClick={() => setIsAudioUpload(false)}>
+            Enter transcript
           </Button>
+          <Button onClick={() => setIsAudioUpload(true)}>Upload Audio</Button>
+          {isAudioUpload ? (
+            <>
+              <FormControl id='file' isRequired>
+                <FormLabel>File Upload</FormLabel>
+                <Input
+                  type='file'
+                  onChange={handleFileChange}
+                  accept='audio/*' // Restrict file types if needed
+                />
+              </FormControl>
+              <Button
+                type='submit'
+                colorScheme='blue'
+                w='full'
+                disabled={isSpeaking}
+              >
+                Generate Podcast
+              </Button>
+            </>
+          ) : (
+            <>
+              <FormControl id='text' isRequired>
+                <FormLabel>Text Input</FormLabel>
+                <Textarea
+                  placeholder='Enter your text here'
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+              </FormControl>
+              <Button
+                type='submit'
+                colorScheme='blue'
+                w='full'
+                disabled={isSpeaking}
+              >
+                {isSpeaking ? 'Speaking...' : 'Speak'}
+              </Button>
+            </>
+          )}
         </VStack>
       </form>
     </Box>
